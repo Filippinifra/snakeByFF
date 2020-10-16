14dda75d19d26b0bfed10cf11c10b65d21441b8d
filippinifra <filippinifra123@gmail.com>
2020-10-17 00:21:21 +0200

    Addding babel config setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            img: "./App/img",
+            screen: "./App/screen",
+            hooks: "./App/hooks",
+            config: "./App/Config",
+          },
+        },
+      ],
+    ],
   };
 };
