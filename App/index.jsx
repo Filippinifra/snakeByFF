d36f190f01dbe2cf98a2117ad49abab2ab422a7e
filippinifra <filippinifra123@gmail.com>
2020-10-17 00:25:58 +0200

    Adding index file in App folder and routes

---
+++ App/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { SnakeScreen } from "./screen/SnakeScreen";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { SnakeConfigScreen } from "screen/SnakeConfigScreen";
+
+const Stack = createStackNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="SnakeHome">
+      <Stack.Screen
+        name="SnakeHome"
+        component={SnakeScreen}
+        options={{
+          headerStyle: { backgroundColor: "#007df7", height: "auto" },
+          title: null,
+        }}
+      />
+      <Stack.Screen
+        name="SnakeConfig"
+        component={SnakeConfigScreen}
+        options={{
+          headerStyle: { backgroundColor: "#007df7", height: "auto" },
+          title: null,
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
