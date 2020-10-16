bd7e26ff27652838ed652bf7e5520d7928831a18
filippinifra <filippinifra123@gmail.com>
2020-10-17 00:24:49 +0200

    Add snake config file

---
+++ App/screen/SnakeScreen/snakeConfig.js
@@ -0,0 +1,14 @@
+export const N_ROW = 10;
+export const N_COL = 10;
+
+export const INITAL_POSITION = [2, 2];
+
+export const INITAL_DIRECTION = "right";
+
+export const INITIAL_SPEED = 300;
+
+export const BORDER_COLOR = "lightgray";
+export const HEAD_COLOR = "black";
+export const TAIL_COLOR = "gray";
+export const APPLE_COLOR = "gold";
+export const SQUARE_COLOR = "white";
