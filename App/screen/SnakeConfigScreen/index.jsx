0b36d957ca3a901f47efae37194f60af62b7cf19
filippinifra <filippinifra123@gmail.com>
2020-10-17 00:26:15 +0200

    Create new screen for snake config

---
+++ App/screen/SnakeConfigScreen/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const SnakeConfigScreen = () => {
+  return (
+    <View>
+      <Text>ciao</Text>
+    </View>
+  );
+};
