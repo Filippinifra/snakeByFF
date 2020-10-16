0d0f698bba4e4c5256be229d34f0631cbd3a3114
filippinifra <filippinifra123@gmail.com>
2020-10-17 00:25:26 +0200

    Add SnakeScreen component as snake logic and render

---
+++ App/screen/SnakeScreen/index.jsx
@@ -0,0 +1,180 @@
+import React, { useState } from "react";
+import {
+  Alert,
+  Dimensions,
+  Linking,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import GestureRecognizer from "react-native-swipe-gestures";
+import _ from "lodash";
+import { Board } from "screen/SnakeScreen/Board";
+import {
+  INITIAL_SPEED,
+  INITAL_DIRECTION,
+  INITAL_POSITION,
+} from "screen/SnakeScreen/snakeConfig";
+import { useInterval } from "hooks/useInterval";
+import {
+  getRandomFreeBox,
+  moveHead,
+  onSwipe,
+  changeApplePosition,
+  moveTail,
+} from "screen/SnakeScreen/gameUtils";
+import { PERSONAL_WEBSIT_URL } from "config/config";
+import {
+  BottomContainer,
+  CentralContainer,
+  CurrentScoreContainer,
+  HighScoreContainer,
+  IconPlay,
+  LogoContainer,
+  LogoImg,
+  MiddleContainer,
+  PoweredLabel,
+  SafeArea,
+  ScoreText,
+  TopContainer,
+  RightTopContainer,
+} from "./styles";
+import { getScore, storeScore } from "./gameUtils";
+import { useEffect } from "react";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
+const SnakeScreen = () => {
+  const [headRow, setHeadRow] = useState(INITAL_POSITION[0]);
+  const [headCol, setHeadCol] = useState(INITAL_POSITION[1]);
+  const [tail, setTail] = useState([]);
+  const [direction, setDirection] = useState(INITAL_DIRECTION);
+  const [apple, setApple] = useState(() =>
+    getRandomFreeBox([headRow, headCol])
+  );
+  const [score, setScore] = useState(0);
+  const [pause, setPause] = useState(true);
+  const [speed, setSpeed] = useState(INITIAL_SPEED);
+  const [directionChanged, setDirectionChanged] = useState(false);
+  const [highScore, setHighScore] = useState(0);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    getScore(setHighScore);
+  }, []);
+
+  const windowHeight = Dimensions.get("window").height;
+
+  const handleGameOver = () => {
+    const isTailOverHead = tail.some(
+      (tailElement) => tailElement[0] === headRow && tailElement[1] === headCol
+    );
+    if (isTailOverHead) {
+      setHeadRow(INITAL_POSITION[0]);
+      setHeadCol(INITAL_POSITION[1]);
+      setTail([]);
+      setDirection(INITAL_DIRECTION);
+      setApple(getRandomFreeBox([[headRow, headCol]]));
+      setPause(true);
+      setSpeed(INITIAL_SPEED);
+      if (score > highScore) {
+        storeScore(score);
+        setHighScore(score);
+      }
+      setScore(0);
+      Alert.alert("GAME OVER", `Yuor score is ${score}`);
+    }
+  };
+
+  const increaseSpeed = (isEating) => {
+    const isNotJustStart = score !== 0;
+    const isLevelUp = score % 10 === 0;
+    if (isEating && isNotJustStart && isLevelUp) {
+      setSpeed(INITIAL_SPEED - score * 3);
+    }
+  };
+
+  const newTurn = () => {
+    if (!pause) {
+      const oldPositionHead = [headRow, headCol];
+      moveHead(direction, setHeadCol, setHeadRow, headCol, headRow);
+      const isEating = headRow === apple[0] && headCol === apple[1];
+      moveTail(isEating, oldPositionHead, tail, setTail);
+      if (isEating) {
+        changeApplePosition(setApple, tail, [headRow, headCol]);
+        setScore(score + 1);
+      }
+      increaseSpeed(isEating);
+      handleGameOver();
+      setDirectionChanged(false);
+    }
+  };
+
+  useEffect(() => {});
+
+  useInterval(newTurn, pause ? null : speed);
+
+  const redirectToWebsite = () => Linking.openURL(PERSONAL_WEBSIT_URL);
+
+  return (
+    <GestureRecognizer
+      onSwipe={(newDirection) => {
+        if (!directionChanged) {
+          onSwipe(newDirection, direction, setDirection);
+          setDirectionChanged(true);
+        }
+      }}
+      config={{
+        velocityThreshold: 0.5,
+        directionalOffsetThreshold: 80,
+      }}
+      style={{ flex: 1 }}
+    >
+      <CentralContainer height={windowHeight}>
+        <Board
+          headPosition={[headRow, headCol]}
+          applePosition={apple}
+          tail={tail}
+        />
+      </CentralContainer>
+      <TopContainer>
+        <MiddleContainer style={{ flexDirection: "row" }}>
+          <CurrentScoreContainer>
+            <Text style={{ position: "absolute", paddingBottom: 50 }}>
+              Score
+            </Text>
+            <ScoreText>{score}</ScoreText>
+          </CurrentScoreContainer>
+          <HighScoreContainer>
+            <Text style={{ position: "absolute", paddingBottom: 55 }}>👑</Text>
+            <ScoreText>{highScore}</ScoreText>
+          </HighScoreContainer>
+        </MiddleContainer>
+        <MiddleContainer>
+          <IconPlay
+            name={pause ? "play-arrow" : "pause"}
+            onPress={() => setPause(!pause)}
+            size={50}
+          />
+        </MiddleContainer>
+      </TopContainer>
+      <BottomContainer>
+        <PoweredLabel onPress={redirectToWebsite}>Powered by</PoweredLabel>
+        <LogoContainer>
+          <TouchableOpacity onPress={redirectToWebsite}>
+            <LogoImg source={require("img/logoFF.png")} />
+          </TouchableOpacity>
+        </LogoContainer>
+      </BottomContainer>
+      <RightTopContainer>
+        <Icon
+          name="settings"
+          size={30}
+          onPress={() => navigation.push("SnakeConfig")}
+        />
+      </RightTopContainer>
+    </GestureRecognizer>
+  );
+};
+
+export { SnakeScreen };
---
+++ App/screen/SnakeScreen/styles.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+import { Icon } from "react-native-elements";
+
+export const LogoImg = styled.Image`
+  margin-top: 4px;
+  height: 30px;
+  width: 30px;
+`;
+
+export const CentralContainer = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: ${({ height }) => height / 2}px;
+`;
+
+export const TopContainer = styled.View`
+  height: 25%;
+  position: absolute;
+  justify-content: center;
+  align-self: center;
+  flex-direction: row;
+`;
+
+export const BottomContainer = styled.View`
+  height: 25%;
+  position: absolute;
+  bottom: 0;
+  justify-content: center;
+  align-self: center;
+  align-items: center;
+`;
+
+export const MiddleContainer = styled.View`
+  width: 50%;
+  align-content: center;
+  justify-content: center;
+`;
+
+export const ScoreText = styled.Text`
+  color: black;
+  font-size: 30px;
+  align-self: center;
+`;
+
+export const IconPlay = styled(Icon)`
+  border-color: #007df7;
+  border-width: 3px;
+  border-radius: 100px;
+  height: 60px;
+  width: 60px;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+`;
+
+export const LogoContainer = styled.View`
+  height: 50px;
+  width: 50px;
+  background-color: #007df7;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+`;
+
+export const PoweredLabel = styled.Text`
+  color: #007df7;
+  font-size: 14px;
+  margin-bottom: 10px;
+`;
+
+export const CurrentScoreContainer = styled.View`
+  justify-content: center;
+  margin-left: 30px;
+  align-self: center;
+  align-items: center;
+  width: 50px;
+`;
+
+export const HighScoreContainer = styled.View`
+  justify-content: center;
+  margin-left: 30px;
+  align-self: center;
+  align-items: center;
+`;
+
+export const RightTopContainer = styled.View`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
