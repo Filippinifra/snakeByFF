f6245768d17c14e30d140d674010c808b00bbd54
filippinifra <filippinifra123@gmail.com>
2020-10-18 17:56:41 +0200

    Update SnakeConfig with available colors

--- src/config/snakeConfig.js
+++ src/config/snakeConfig.js
@@ -8,7 +8,20 @@ export const INITAL_DIRECTION = "right";
 export const INITIAL_SPEED = 300;
 
 export const BORDER_COLOR = "lightgray";
-export const HEAD_COLOR = "black";
-export const TAIL_COLOR = "gray";
-export const APPLE_COLOR = "gold";
 export const SQUARE_COLOR = "white";
+
+export const INITIAL_HEAD_COLOR = "black";
+export const INITIAL_TAIL_COLOR = "gray";
+export const INITIAL_APPLE_COLOR = "gold";
+
+export const AVAILABLE_COLORS = [
+  "black",
+  "gray",
+  "yellow",
+  "gold",
+  "orange",
+  "red",
+  "purple",
+  "blue",
+  "green",
+];
