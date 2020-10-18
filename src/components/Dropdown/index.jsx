6062f9c473d484a2a7e3d04226d907e422e29da6
filippinifra <filippinifra123@gmail.com>
2020-10-18 17:56:21 +0200

    Update Dropdown component

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import RNPickerSelect from "react-native-picker-select";
+import { DropdownWrapper } from "./styles";
 
-export const Dropdown = () => (
-  <RNPickerSelect
-    onValueChange={() => {}}
-    items={[
-      { label: "Football", value: "football" },
-      { label: "Baseball", value: "baseball" },
-      { label: "Hockey", value: "hockey" },
-    ]}
-  />
+export const Dropdown = ({ style, items, onChange, value }) => (
+  <DropdownWrapper style={style}>
+    <RNPickerSelect onValueChange={onChange} value={value} items={items} />
+  </DropdownWrapper>
 );
---
+++ src/components/Dropdown/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const DropdownWrapper = styled.View`
+  border-width: 1px;
+  border-radius: 50px;
+  padding: 5px 10px;
+`;
